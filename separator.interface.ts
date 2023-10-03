e09f4e0d7443d4fd430dc5d293748ef5c1c652a5
VanyaMate <buhu653@mail.ru>
2023-10-03 04:09:58 +0300

    fix interface

--- separator.interface.ts
+++ separator.interface.ts
@@ -5,20 +5,22 @@ export type SeparatorOption = {
 export type AdditionalSeparatorOption<R> =
     SeparatorOption
     & {
-        temp: R[],
-        start: number
-    };
+    temp: R[],
+    start: number
+};
 
 export type SortSeparatorOption<T> =
     SeparatorOption
     & {
-        temp: T[],
-        max: T,
-        sorted: number,
-        checked: number,
-    }
+    temp: T[],
+    max: T,
+    sorted: number,
+    checked: number,
+}
 
 export interface ISeparator {
+    findFirst<T> (array: T[], searchCallback: (T) => boolean, options: SeparatorOption): Promise<T>;
+
     map<T, R> (array: T[], mapCallback: (T) => R, options: SeparatorOption): Promise<R[]>;
 
     filter<T> (array: T[], filterCallback: (T) => boolean, options: SeparatorOption): Promise<T[]>;
